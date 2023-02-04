a46dd6f8df93d86eceadbca25847c9d9d1459d97
“Shawna <shawna.e.hartley@gmail.com>
2023-02-04 17:51:07 -0500

    setup complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+  before_action :snake_case_params
+
+
+
+  private
+  # converts keys from camelCase to snake_case
+  def snake_case_params
+    params.deep_transform_keys!(&:underscore)
+  end
+
+
 end
