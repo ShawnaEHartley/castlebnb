b6fe6915a75a1ebfe9363953f4b09642ab438c64
“Shawna <shawna.e.hartley@gmail.com>
2023-02-10 18:00:39 -0500

    listings rendered on page

--- frontend/src/components/ListingIndex/ListingIndex.js
+++ frontend/src/components/ListingIndex/ListingIndex.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchListings, getListings } from '../../store/listings';
+import image from '../../assets/images/image-coming-soon.png'
+
+import './ListingIndex.css'
 
 
 
@@ -15,8 +18,19 @@ const ListingIndex = () => {
   console.log(listings)
 
   return (
-    <h3>This is the listing index</h3>
-
+    <div className="index">
+      {listings.map((listing) => {
+        return (
+          <div className='index-item'>
+            <img className='index-image'src={image} alt='Default'></img>
+            <h1 className='index-header'>{listing.city}, {listing.region}</h1>
+            <p className='info-line-1'>placeholder</p>
+            <p className='info-line-2'>placeholder</p>
+            <p className='index-price'>$ {listing.price} night</p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
